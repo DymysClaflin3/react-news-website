88c615fb62cf599293feaea30fe311b2d553ef17
DymysClaflin3 <dymys.claflin3@gmail.com>
2023-05-17 23:32:30 -0700

    Initialized project

---
+++ src/components/News/Sidenews.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import SingleSide from './SingleSide';
+import Error from './Error';
+
+class Sidenews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidenews: [],
+      error: false,
+    };
+  }
+
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=a9e7c4f2e4ea4e1daa894ff2a50de8d3`;
+
+    axios.get(url)
+      .then((response) => {
+        this.setState({
+          sidenews: response.data.articles
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error: true
+        })
+      });
+  }
+
+  renderItems() {
+    if (!this.state.error) {
+      return this.state.sidenews.map((item) => (
+        <SingleSide key={item.url} item={item} />
+      ));
+    } else {
+      return <Error />
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderItems()}
+      </div>
+    );
+  }
+}
+
+export default Sidenews;
